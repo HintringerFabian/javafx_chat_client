a6a1f128068eb4cc5f363c4a63ccc7f231815f62
Fabian Hintringer <fabianhintringer01@gmail.com>
2023-05-26 00:45:05 +0200

    here we go again

--- chat_client/common/src/main/swe4/common/Message.java
+++ chat_client/common/src/main/swe4/common/Message.java
@@ -5,6 +5,7 @@ import java.io.Serializable;
 public class Message implements Serializable {
 	// TODO maybe next time dont forget to implement timestamps
 	// TODO also add them to the view
+	// TODO 26.05.2023 i forgot to add timestamps
 	private final User user;
 	private final String message;
 
